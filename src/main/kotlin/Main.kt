1df091ca835e4a194ef2a0f7007f95b9a7bda2db
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-04-12 21:56:11 +0300

    added lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,46 @@ fun main() {
     numbers()
     serveDrinksByAge(15)
 
+
+
+val friends = listOf("Fridah","terry","mary","tom")
+println(friends.contains("mary"))
+
+
+val nums = listOf(-1,78,-45,87,32,0,7,-97)
+/*Apredicate is like a seive == the condition*/
+val negativeNums = nums.filter { num->num<0 }
+println(negativeNums)
+
+val names = listOf("ben","ken","ten","pen","pen","pen","ken")
+val realNames = names.filter{name-> name!="ten"&& name != "pen"}
+println(realNames)
+
+val contact1=Contact("Brian","0765654356","brian@gmail.com","male")
+val contact2 = Contact("Penina","0789898989","penina@gmail.com","female")
+val contact3 =Contact("Auma","012345345678","nyambufrtd","female")
+val contact4 =Contact("Tito","012345345678","nyambufrtd","male")
+
+val myContacts = listOf(contact1,contact2,contact3,contact4)
+val femaleContacts = myContacts.filter{contact->contact.gender=="female"}
+val shortContacts = myContacts.filter{contact->contact.name.length==4}
+println(femaleContacts)
+    println(femaleContacts)
+
+
+
+    val words = listOf("apple", "banana", "orange", "pear", "kiwi", "grape")
+
+    val vowelList = words.filter { it.startsWith("a") || it.startsWith("e")
+            || it.startsWith("i") || it.startsWith("o") || it.startsWith("u") }
+    val consonantList = words.filter { !it.startsWith("a") && !it.startsWith("e")
+            && !it.startsWith("i") && !it.startsWith("o") && !it.startsWith("u") }
+
+    println("Words starting with a vowel: $vowelList")
+    println("Words starting with a consonant: $consonantList")
 }
+
+
 fun printOddNumbers() {
         for (i in 1..100) {
             if (i % 2 != 0) {
@@ -40,4 +79,18 @@ fun numbers(){
             println("FizzBuzz")
         }
     }}
+
+data class Contact(
+    var name:String,
+    var phoneNumber:String,
+    var email:String,
+    var gender:String)
+
+
+
+
+
+
+
+
 
